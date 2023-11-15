f151809a5f600f8316316d1e8e387d2a9e35fe1e
Ousseyn <osakho62@gmail.com>
2023-11-15 12:27:40 +0100

    packages and controller creation

---
+++ src/main/java/fr/efrei/server/web/rest/ItemController.java
@@ -0,0 +1,9 @@
+package fr.efrei.server.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ItemController {
+}
