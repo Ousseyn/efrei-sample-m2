ff5982653d22cf9ae91de7f73712a7bf23a0891e
Ousseyn <osakho62@gmail.com>
2023-11-27 10:34:27 +0100

    Just some comments

--- src/main/java/fr/efrei/server/web/rest/ItemResource.java
+++ src/main/java/fr/efrei/server/web/rest/ItemResource.java
@@ -1,4 +1,4 @@
-package fr.efrei.server.web.rest;
+package fr.efrei.server.web.rest; //Le nom de mon server
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
